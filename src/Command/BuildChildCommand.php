7fc8f32b615cd9a307cca693e73579a89e023eb9
Arne von Appen <arne.appen@limetec-biotechnologies.de>
2017-01-05 16:13:55 +0100

    first commit

--- src/Command/BuildChildCommand.php
+++ src/Command/BuildChildCommand.php
@@ -22,12 +22,82 @@ class BuildChildCommand extends Command {
 
     protected function execute (InputInterface $input, OutputInterface $output) {
         $repo = new Repository(getcwd());
+
         try {
-            $output->writeln($repo->getMainBranch());
-            $output->writeln(getcwd());
-            $output->writeln('hello world');
+            $branch = $repo->getMainBranch();
+
+            if ($this->isBuildBranch($branch->getName())) {
+                $output->writeln('<info>build branch '.$branch->getName().'</info>');
+
+                if ($this->isChildBranch($branch->getName())) {
+                    $nextName = '';
+                    if ($branch->getName() == 'master') {
+                        $nextName = $this->getNextMasterChild($repo);
+                    }
+                    $output->writeln('<info>create a new build branch '.$nextName.'...</info>');
+
+                    $repo->createBranch($nextName);
+                    $repo->commit('commit generated branch');
+                    $repo->push();
+                } else {
+                    $output->writeln('<info>create a new build tag</info>');
+                }
+            } else {
+                $output->writeln('<comment>branch is no build branch: '.$branch->getName().
+                                 '. nothing to doo</comment>');
+            }
         } catch (\RuntimeException $e) {
             $output->writeln('<error>'.$e->getMessage().'</error>');
         }
+    }
+
+    private function getNextMasterChild (Repository $repository) {
+        $versions = [];
+        foreach ($repository->getBranches(true, true) as $branch) {
+            preg_match("/[0-9]*/", $branch, $output_array);
+            if ($output_array[0] == $branch) {
+                $versions[intval($branch)] = $branch;
+            }
+        }
+
+        return (string) count($versions);
+    }
+
+//    private function getNextBranchChild (Repository $repository) {
+//        $versions = [];
+//        $mainVersion = $repository->getMainBranch()
+//                                  ->getName();
+//
+//        foreach ($repository->getBranches(true, true) as $branch) {
+//            preg_match("/".$mainVersion.".([0-9]*)/", $branch, $output_array);
+//        }
+//    }
+
+    private function isChildBranch ($branchName) {
+        if ($branchName == 'master') {
+            return true;
+        } else {
+            preg_match("/[0-9]*/", $branchName, $output_array);
+            if ($output_array[0] == $branchName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isBuildBranch ($branchName) {
+        $buildBranch = false;
+
+        if ($branchName == 'master') {
+            $buildBranch = true;
+        } else {
+            preg_match("/[0-9]*[.[0-9]*]?/", $branchName, $output_array);
+            if ($output_array[0] == $branchName) {
+                $buildBranch = true;
+            }
+        }
+
+        return $buildBranch;
     }
 }
