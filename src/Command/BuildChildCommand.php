3cd855faad0e8967689a59d261221ead1e11ccaa
Arne von Appen <arne.appen@limetec-biotechnologies.de>
2017-01-23 13:28:57 +0100

    fix regex

--- src/Command/BuildChildCommand.php
+++ src/Command/BuildChildCommand.php
@@ -16,26 +16,23 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class BuildChildCommand extends BaseCommand
-{
-    protected function configure()
-    {
+class BuildChildCommand extends BaseCommand {
+    protected function configure () {
         $this->setName('build:child')
-            ->addOption('server-url',
-                's',
-                InputOption::VALUE_REQUIRED,
-                'the gitlab server-url. for example: http://your.domain/api/v3/')
-            ->addOption('auth-token', 't', InputOption::VALUE_REQUIRED, 'your gitlab user token')
-            ->addOption('repository', 'r', InputOption::VALUE_REQUIRED, 'the repository name')
-            ->addOption('branch', 'b', InputOption::VALUE_REQUIRED, 'the branch or tag name')
-            ->setDescription('create child branch/tag');
+             ->addOption('server-url',
+                         's',
+                         InputOption::VALUE_REQUIRED,
+                         'the gitlab server-url. for example: http://your.domain/api/v3/')
+             ->addOption('auth-token', 't', InputOption::VALUE_REQUIRED, 'your gitlab user token')
+             ->addOption('repository', 'r', InputOption::VALUE_REQUIRED, 'the repository name')
+             ->addOption('branch', 'b', InputOption::VALUE_REQUIRED, 'the branch or tag name')
+             ->setDescription('create child branch/tag');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute (InputInterface $input, OutputInterface $output) {
         /* @var $factory \ApplicationFactory */
         $factory = $this->getContainer()
-            ->get('factory');
+                        ->get('factory');
 
         $client = $factory->createClient($input->getOption('server-url'));
         $client->authenticate($input->getOption('auth-token'), Client::AUTH_URL_TOKEN);
@@ -43,7 +40,8 @@ class BuildChildCommand extends BaseCommand
         $branchName = $input->getOption('branch');
 
         try {
-            $projectJson = $client->api('projects')->search($input->getOption('repository'));
+            $projectJson = $client->api('projects')
+                                  ->search($input->getOption('repository'));
             if (count($projectJson) != 1) {
                 throw new RuntimeException('cant identify project');
             }
@@ -53,34 +51,35 @@ class BuildChildCommand extends BaseCommand
             $tags = $project->tags();
 
             if ($this->isBuildBranch($branchName)) {
-                $output->writeln('<info>build branch ' . $branchName . '</info>');
+                $output->writeln('<info>build branch '.$branchName.'</info>');
                 if ($this->isChildBranch($branchName)) {
                     if ($branchName == 'master') {
                         $nextName = $this->getNextMasterChild($branches);
                     } else {
-                        $nextName = $branchName . '.' . $this->getNextBranchChild($branches, $branchName);
+                        $nextName = $branchName.'.'.$this->getNextBranchChild($branches, $branchName);
                     }
-                    $output->writeln('<info>create a new minor/major branch ' . $nextName . '...</info>');
+                    $output->writeln('<info>create a new minor/major branch '.$nextName.'...</info>');
                     $project->createBranch($nextName, $branchName);
                 } else {
-                    $tagName = 'v' . $branchName . '.' . $this->getNextBranchTag($tags, $branchName);
-                    $output->writeln('<info>create a new release tag ' . $tagName . '...</info>');
-                    $client->api('repositories')->createTag($projectJson[0]['id'], $tagName, $branchName);
+                    $tagName = 'v'.$branchName.'.'.$this->getNextBranchTag($tags, $branchName);
+                    $output->writeln('<info>create a new release tag '.$tagName.'...</info>');
+                    $client->api('repositories')
+                           ->createTag($projectJson[0]['id'], $tagName, $branchName);
                 }
             } else {
-                $output->writeln('<info>branch is no minor/major branch: ' . $branchName . '. nothing to doo</info>');
+                $output->writeln('<info>branch is no minor/major branch: '.$branchName.'. nothing to doo</info>');
             }
         } catch (RuntimeException $e) {
-            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            $output->writeln('<error>'.$e->getMessage().'</error>');
         }
     }
 
     /**
      * @param Branch[] $branches
+     *
      * @return string
      */
-    private function getNextMasterChild(array $branches)
-    {
+    private function getNextMasterChild (array $branches) {
         $versions = [];
         foreach ($branches as $branch) {
             preg_match("/[0-9]*/", $branch->name, $output_array);
@@ -89,60 +88,59 @@ class BuildChildCommand extends BaseCommand
             }
         }
 
-        return (string)count($versions);
+        return (string) count($versions);
     }
 
     /**
      * @param Branch[] $branches
-     * @param $mainVersion
+     * @param          $mainVersion
+     *
      * @return string
      */
-    private function getNextBranchChild(array $branches, $mainVersion)
-    {
+    private function getNextBranchChild (array $branches, $mainVersion) {
         $versions = [];
 
         foreach ($branches as $branch) {
-            preg_match("/" . $mainVersion . ".([0-9]*)/", $branch->name, $output_array);
+            preg_match("/".$mainVersion.".([0-9]*)/", $branch->name, $output_array);
             if (count($output_array) > 0) {
                 $versions[intval($output_array[1])] = $output_array[0];
             }
         }
 
-        return (string)count($versions);
+        return (string) count($versions);
     }
 
     /**
      * @param Tag[] $tags
-     * @param $branchName
+     * @param       $branchName
+     *
      * @return string
      */
-    private function getNextBranchTag(array $tags, $branchName)
-    {
+    private function getNextBranchTag (array $tags, $branchName) {
         $versions = [];
 
         preg_match("/([0-9]*).([0-9]*)/",
-            $branchName,
-            $output_array);
+                   $branchName,
+                   $output_array);
         $mainVersion = $output_array[1];
         $secVersion = $output_array[2];
 
         foreach ($tags as $tag) {
-            preg_match("/v" . $mainVersion . "." . $secVersion . ".([0-9]*)/", $tag->name, $output_array);
+            preg_match("/v".$mainVersion.".".$secVersion.".([0-9]*)/", $tag->name, $output_array);
             if (count($output_array) > 1) {
                 $versions[intval($output_array[1])] = $output_array[0];
             }
         }
 
-        return (string)count($versions);
+        return (string) count($versions);
     }
 
-    private function isChildBranch($branchName)
-    {
+    private function isChildBranch ($branchName) {
         if ($branchName == 'master') {
             return true;
         } else {
             preg_match("/^[0-9]*$/", $branchName, $output_array);
-            if ($output_array[0] === $branchName) {
+            if (count($output_array) > 0 && $output_array[0] === $branchName) {
                 return true;
             }
         }
@@ -150,15 +148,14 @@ class BuildChildCommand extends BaseCommand
         return false;
     }
 
-    private function isBuildBranch($branchName)
-    {
+    private function isBuildBranch ($branchName) {
         $buildBranch = false;
 
         if ($branchName == 'master') {
             $buildBranch = true;
         } else {
             preg_match("/^[0-9]*[.[0-9]*]?$/", $branchName, $output_array);
-            if ($output_array[0] == $branchName && $branchName != '') {
+            if (count($output_array) > 0 && $output_array[0] == $branchName && $branchName != '') {
                 $buildBranch = true;
             }
         }
